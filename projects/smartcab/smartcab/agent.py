7ae9ea00a287056c3502336d1ac8d7a0345f28f2
NELSON VEGA <nelsoN@kleiotechnology.com>
2019-05-17 21:18:57 -0400

    A/A+

--- projects/smartcab/smartcab/agent.py
+++ projects/smartcab/smartcab/agent.py
@@ -182,7 +182,7 @@ def run():
     #   learning   - set to True to force the driving agent to use Q-learning
     #    * epsilon - continuous value for the exploration factor, default is 1
     #    * alpha   - continuous value for the learning rate, default is 0.5
-    agent = env.create_agent(LearningAgent,learning=True,epsilon=1.3,alpha=0.6)
+    agent = env.create_agent(LearningAgent,learning=True,epsilon=0.8,alpha=0.4)
 
     ##############
     # Follow the driving agent
@@ -204,7 +204,7 @@ def run():
     # Flags:
     #   tolerance  - epsilon tolerance before beginning testing, default is 0.05
     #   n_test     - discrete number of testing trials to perform, default is 0
-    sim.run(n_test=20)
+    sim.run(n_test=10)
 
 
 if __name__ == '__main__':
